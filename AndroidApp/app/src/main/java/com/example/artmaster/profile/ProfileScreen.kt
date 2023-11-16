33bfb4cd62cb7c31c347d7a800fc192c3110279b
BraianOviedo <braian_oviedo@gmx.com>
2023-11-16 20:01:03 -0300

    Profile: User can edit their name and Log out

--- AndroidApp/app/src/main/java/com/example/artmaster/profile/ProfileScreen.kt
+++ AndroidApp/app/src/main/java/com/example/artmaster/profile/ProfileScreen.kt
@@ -1,10 +1,15 @@
 package com.example.artmaster.profile
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.ExitToApp
@@ -16,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.unit.dp
@@ -23,6 +29,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.artmaster.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 @Composable
 fun ProfileScreen(
@@ -30,7 +37,6 @@ fun ProfileScreen(
     navigateToLogin: () -> Unit
 ) {
 
-    val auth = FirebaseAuth.getInstance()
     val user = dataViewModel.state.value
 
     Column(
@@ -38,25 +44,20 @@ fun ProfileScreen(
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        ProfileHeader(user)
+        ProfileHeader(user, navigateToLogin)
         Spacer(modifier = Modifier.height(16.dp))
-        ProfileInfoItem(Icons.Default.Person, "Nombre", user.name)
+        ProfileInfoItem(Icons.Default.Person, "Nombre", user.name, true, onEditClick = {})
         Spacer(modifier = Modifier.height(8.dp))
-        ProfileInfoItem(Icons.Default.Email, "Correo Electrónico", user.email)
+        ProfileInfoItem(Icons.Default.Email, "Correo Electrónico", user.email, false)
         Spacer(modifier = Modifier.height(32.dp))
-        ProfileButton(Icons.Default.Edit, "Editar Perfil") {
-            // Aquí deberías abrir la pantalla de edición de perfil
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        ProfileButton(Icons.Default.ExitToApp, "Cerrar Sesión") {
-            auth.signOut()
-            navigateToLogin()
-        }
     }
 }
 
 @Composable
-fun ProfileHeader(user: User) {
+fun ProfileHeader(user: User, navigateToLogin: () -> Unit) {
+    val auth = FirebaseAuth.getInstance()
+
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -71,7 +72,7 @@ fun ProfileHeader(user: User) {
                 .clip(CircleShape)
         )
         Spacer(modifier = Modifier.width(16.dp))
-        Column {
+        Column(modifier = Modifier.weight(1f)) {
             Text(
                 text = user.name,
                 fontSize = 18.sp,
@@ -85,11 +86,38 @@ fun ProfileHeader(user: User) {
                 color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
             )
         }
+        Spacer(modifier = Modifier.width(16.dp))
+        IconButton(onClick = {
+            auth.signOut()
+            navigateToLogin()
+        }) {
+            Icon(
+                imageVector = Icons.Default.ExitToApp,
+                contentDescription = "Cerrar Sesion",
+                tint = Color.Red,
+                modifier = Modifier
+                    .size(32.dp)
+                    .padding(4.dp)
+                )
+        }
     }
 }
 
 @Composable
-fun ProfileInfoItem(icon: ImageVector, label: String, value: String) {
+fun ProfileInfoItem(
+    icon: ImageVector,
+    label: String,
+    value: String,
+    isEditable: Boolean = false,
+    onEditClick: ((String) -> Unit)? = null
+) {
+    var isEditing by remember { mutableStateOf(false) }
+    var editedValue by remember { mutableStateOf(value) }
+    val auth = FirebaseAuth.getInstance()
+    val currentUser = auth.currentUser
+    val userId = currentUser?.uid
+    val context = LocalContext.current
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -112,34 +140,96 @@ fun ProfileInfoItem(icon: ImageVector, label: String, value: String) {
                 color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = value,
-                fontSize = 16.sp,
-                color = MaterialTheme.colorScheme.onSurface
-            )
+            if (isEditing) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    BasicTextField(
+                        value = editedValue,
+                        onValueChange = { editedValue = it },
+                        textStyle = LocalTextStyle.current.copy(fontSize = 16.sp),
+                        modifier = Modifier
+                            .weight(1f)
+                            .background(
+                                color = MaterialTheme.colorScheme.onSurface,
+                                RoundedCornerShape(4.dp)
+                            )
+                            .padding(4.dp)
+                    )
+                    IconButton(onClick = {
+                        if (editedValue.length in 3..24) {
+                            // Save edit on Firestore
+                            onEditClick?.invoke(editedValue)
+
+                            // Update the field on Firestore
+                            userId?.let { userId ->
+                                updateUserField(userId, "name", editedValue)
+                            }
+
+                            Toast.makeText(
+                                context,
+                                "Nombre editado",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            isEditing = false
+                        } else {
+                            Toast.makeText(
+                                context,
+                                "El nombre debe tener entre 3 y 24 caracteres.",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    }
+                }
+            } else {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(
+                        text = value,
+                        fontSize = 16.sp,
+                    )
+                    if (isEditable) {
+                        IconButton(onClick = {
+                            isEditing = true
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Edit,
+                                contentDescription = null,
+                                modifier = Modifier.size(16.dp)
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
-@Composable
-fun ProfileButton(icon: ImageVector, label: String, onClick: () -> Unit) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier.fillMaxWidth(),
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Icon(imageVector = icon, contentDescription = null)
-            Text(
-                text = label,
-                fontSize = 14.sp,
-                modifier = Modifier.padding(start = 8.dp)
-            )
+
+
+
+fun updateUserField(userId: String, field: String, value: String) {
+    val db = FirebaseFirestore.getInstance()
+    val userRef = db.collection("usuarios").document(userId)
+
+    userRef
+        .update(field, value)
+        .addOnSuccessListener {
+            // Update successful
+            Log.d("Firestore", "Campo actualizado correctamente: $field")
         }
-    }
+        .addOnFailureListener { e ->
+            // Error update
+            Log.w("Firestore", "Error al actualizar el campo $field", e)
+        }
 }
