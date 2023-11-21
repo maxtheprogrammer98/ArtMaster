55c1c68862a32be1758b0f27ade51e98a65deb14
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-11-21 17:10:15 -0300

    Se agrega seccion home
    Solamente falta agregar animacion sobre slideshow y seleccionar mejores colores (esto podria arreglarse cambiando el tema)

--- AndroidApp/app/src/main/java/com/example/artmaster/home/Home.kt
+++ AndroidApp/app/src/main/java/com/example/artmaster/home/Home.kt
@@ -3,19 +3,34 @@ package com.example.artmaster.home
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artmaster.MainActivity
 import com.example.artmaster.ui.theme.ArtMasterTheme
 import com.example.artmaster.R
@@ -43,14 +58,40 @@ class Home : MainActivity(){
         R.mipmap.portada3
     )
 
-    val sectionsRefe = listOf<SectionsGenerator>()
-
     @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @Composable
     fun CreateHome(){
         // remember variables
         val scrollableSateVertical = rememberScrollState()
+        
+        //sections description
+        val rutas = SectionsGenerator(
+            title = stringResource(id = R.string.rutas),
+            text = stringResource(id = R.string.seccion_rutas),
+            image = painterResource(id = R.mipmap.articon)
+        )
+
+        val favs = SectionsGenerator(
+            title = stringResource(id = R.string.favoritos),
+            text = stringResource(id = R.string.seccion_favoritos),
+            image = painterResource(id = R.mipmap.fav)
+        )
+
+        val notes = SectionsGenerator(
+            title = stringResource(id = R.string.notas),
+            text = stringResource(id = R.string.seccion_notas),
+            image = painterResource(id = R.mipmap.notesicon)
+        )
+
+        val perfil = SectionsGenerator(
+            title = stringResource(id = R.string.perfil),
+            text = stringResource(id = R.string.seccion_perfil),
+            image = painterResource(id = R.mipmap.usericon)
+        )
+
+        // list with all the section descriptions
+        val listaSections = listOf<SectionsGenerator>(rutas,favs,notes,perfil)
 
         // ------------------------ SCAFFOLD MAIN LAYOUT ---------------------------//
         Scaffold(
@@ -76,6 +117,55 @@ class Home : MainActivity(){
                 InsertSlideshow(
                     imagesList = imagesList,
                     context = applicationContext)
+
+                // wrapper
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp,0.dp,0.dp,70.dp)
+                ){
+                    //insterting descriptions dinamically
+                    listaSections.forEach {
+                        // main container
+                            elem -> Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(200.dp, 400.dp)
+                            .wrapContentWidth(Alignment.CenterHorizontally)
+                            .padding(12.dp)
+                            .background(Color.LightGray)
+
+                    ){
+                        // logo
+                        Image(
+                            painter = elem.image ,
+                            contentDescription = elem.title,
+                            modifier = Modifier
+                                .height(100.dp)
+                                .align(Alignment.CenterHorizontally))
+
+                        // title
+                        Text(
+                            text = elem.title,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(0.dp, 6.dp),
+                            textAlign = TextAlign.Center,
+                            fontSize = 20.sp,
+                            textDecoration = TextDecoration.Underline,
+                            fontWeight = FontWeight.Bold)
+
+                        //text / content
+                        Text(
+                            text = elem.text,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(20.dp),
+                            textAlign = TextAlign.Center,
+                            fontSize = 16.sp)
+                    }
+                    }
+                }
             }
         }
     }
--- AndroidApp/app/src/main/java/com/example/artmaster/home/SectionsGenerator.kt
+++ AndroidApp/app/src/main/java/com/example/artmaster/home/SectionsGenerator.kt
@@ -1,12 +1,12 @@
 package com.example.artmaster.home
 
 import android.content.Intent
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class SectionsGenerator(
     //Attributes
     val title:String,
     val text:String,
-    val image:ImageVector,
-    val intent: Intent
+    val image:Painter
 )
