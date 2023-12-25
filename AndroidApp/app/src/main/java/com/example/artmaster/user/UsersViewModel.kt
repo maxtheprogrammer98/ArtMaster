4821523912e69f89d2f3937f601980d0b87bbf6e
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-12-25 16:07:50 -0300

    Agregando control errores en VM users

--- AndroidApp/app/src/main/java/com/example/artmaster/user/UsersViewModel.kt
+++ AndroidApp/app/src/main/java/com/example/artmaster/user/UsersViewModel.kt
@@ -8,10 +8,11 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestoreException
 
 class UsersViewModel : ViewModel(), GetUserID{
     // variable that stores user's ID
-    val userID = getCurrentUserID()
+    val userID = ""
     // mutable variable that stores fetched data
     var userStateProfile = mutableStateOf(UserModels())
 
@@ -28,16 +29,21 @@ class UsersViewModel : ViewModel(), GetUserID{
         val db = Firebase.firestore
         // collection reference
         val usersCollection = db.collection("usuarios")
-        // get request
-        usersCollection
-            .whereEqualTo("id", userID)
-            .get()
-            .await()
-            .map {
-                Log.i("fetch request", userID)
-                val result = it.toObject(UserModels::class.java).copy(id = userID)
-                userProfile = result
-            }
+        // get request (applying handling error structures)
+        try {
+            usersCollection
+                .whereEqualTo("id",userID)
+                .get()
+                .await()
+                .map{
+                    // deserializing object
+                    val result = it.toObject(UserModels::class.java)
+                    userProfile= result
+                }
+
+        } catch (e : FirebaseFirestoreException){
+            Log.e("error_vm_user", "error trying to fetch user model", e)
+        }
         // returning statement
         return userProfile
     }
