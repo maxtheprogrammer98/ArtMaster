62360a8ed8f37356fe4cff223a509d1f750d2481
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-12-26 17:30:47 -0300

    Se agrega activity para visualiar el contenido del intent

--- AndroidApp/app/src/main/java/com/example/artmaster/tutorials/Content.kt
+++ AndroidApp/app/src/main/java/com/example/artmaster/tutorials/Content.kt
@@ -1,12 +1,58 @@
 package com.example.artmaster.tutorials
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.artmaster.R
 
+/**
+ * Creates front page, using Picasso to render the image fetched from DB
+ */
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-fun TutorialPortada(){
+fun addPortada(linkImg : String){
+    //TODO: Install picasso to render images
+}
 
+/**
+ * Adds all the information regarding the tutorial
+ */
+@Composable
+fun AddTutorialContent(
+    nombre: String,
+    descripcion: String,
+    informacion : String,
+    calificacion: Float,
+){
+    // --------------------- CONTENEDOR GENERAL -----------------------//
+    //TODO: improve styling
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ){
+        // ------------ TITULO ---------------//
+        Text(text =  nombre)
+
+        // ------------ DESCRIPTION ---------------//
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = stringResource(id = R.string.descripcion))
+            Text(text = descripcion)
+        }
+
+        // ------------ INFORMATION ---------------//
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = stringResource(id = R.string.informacion))
+            Text(text = informacion)
+        }
+    }
 }
--- AndroidApp/app/src/main/java/com/example/artmaster/tutorials/TutorialsContentActivity.kt
+++ AndroidApp/app/src/main/java/com/example/artmaster/tutorials/TutorialsContentActivity.kt
@@ -1,8 +1,12 @@
 package com.example.artmaster.tutorials
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
@@ -13,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.artmaster.MainActivity
 import com.example.artmaster.R
+import java.io.Serializable
 
 /**
  * this activity contains the tutorials content, from the main activity
@@ -22,12 +27,33 @@ class TutorialsContentActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // avoiding deprecated alert
+        fun <T : Serializable?> getSerializable(activity: Activity, name: String, clazz: Class<T>): T
+        {
+            return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+                activity.intent.getSerializableExtra(name, clazz)!!
+            else
+                activity.intent.getSerializableExtra(name) as T
+        }
+
+        //getting intent data
+        val tutorialData = getSerializable(this, "TUTORIAL_DATA", TutorialsModels::class.java)
+
         setContent {
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ){
-                TutorialsLayoutContent()
+                // null safety check
+                if (tutorialData != null){
+                    Log.i("tuto_content", tutorialData.nombre)
+                   TutorialsLayoutContent(tutorialModels = tutorialData)
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "no ha sido posible obtener la info del tutorial",
+                        Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
@@ -35,7 +61,7 @@ class TutorialsContentActivity : MainActivity() {
     @Composable
     @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    fun TutorialsLayoutContent(){
+    fun TutorialsLayoutContent(tutorialModels: TutorialsModels){
         /* ------------------ MAIN LAYOUT ----------------------- */
         Scaffold(
             topBar = {
@@ -48,6 +74,11 @@ class TutorialsContentActivity : MainActivity() {
                 .fillMaxSize()
         ){
             /* ------------------ CONTENT ----------------------- */
+            AddTutorialContent(
+                nombre = tutorialModels.nombre,
+                descripcion = tutorialModels.descripcion,
+                informacion = tutorialModels.informacion,
+                calificacion = tutorialModels.calificacion)
         }
     }
 
